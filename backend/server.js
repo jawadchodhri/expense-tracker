f341a8dbe9589ad3fd9f441f7788f9a46d16b331
Jawad Chodhri <152736441+jawadchodhri@users.noreply.github.com>
2026-08-12 15:28:36 +0500

    Backend added

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(
+  cors({ origin: "http://localhost:3000" }),
+);
+
+app.use(express.json());
+
+app.get("/api/health", function (request, response) {
+  response.json({ message: "Expense Tracker backend is working." });
+});
+
+app.listen(port, function () {
+  console.log(
+    `Backend is running at http://localhost:${port}`,
+  );
+});
