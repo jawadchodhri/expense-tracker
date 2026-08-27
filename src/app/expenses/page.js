3454fdb80e3604d921e0209c2f6aaea178315207
Jawad Chodhri <152736441+jawadchodhri@users.noreply.github.com>
2026-08-27 18:15:52 +0500

    Update page.js

--- src/app/expenses/page.js
+++ src/app/expenses/page.js
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import Navbar from "@/components/Navbar";
 import ExpenseForm from "@/components/ExpenseForm";
 import TransactionList from "@/components/TransactionList";
-import { saveExpenses } from "@/lib/storage";
 import { calculateTotal, calculateAccountBalance } from "@/lib/Calculation";
 
 export default function ExpensePage() {
