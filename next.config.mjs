37fa93c822ec71443a99a9b65b3273a310d01bdf
Jawad Chodhri <152736441+jawadchodhri@users.noreply.github.com>
2026-09-04 10:48:20 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,12 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*",
+        // When running locally on your laptop, it goes to localhost:5000
+        // When running on Vercel, it forwards to your live Render backend!
+        destination:
+          process.env.NODE_ENV === "production"
+            ? "https://expense-tracker-backend-9yqm.onrender.com"
+            : "http://localhost:5000/api/:path*",
       },
     ];
   },
